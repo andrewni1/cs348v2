eff63fb975d1b506b4c031db44e14689d013b416
unknown <ni95@purdue.edu>
2024-04-24 21:53:10 -0400

    Testting backend

--- server/index.js
+++ server/index.js
@@ -27,3 +27,7 @@ const PORT = process.env.PORT || 5000
 mongoose.connect(CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((err) => console.log(err.message))
+
+app.get("/", (req, res)=> {
+    res.json("Backend works")
+})
